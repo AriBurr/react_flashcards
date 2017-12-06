77f3ad982a1c88062e9a51d1a8c64ac1a94d7b87
Ariel Burriss <ariburr@gmail.com>
2017-12-06 15:02:51 -0700

    init commit react setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FlashApp from './FlashApp'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>React Flashcards</h2>
+        <FlashApp />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/FlashApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class FlashApp extends React.Component {
+  render () {
+
+  }
+}
+
+export default FlashApp;
